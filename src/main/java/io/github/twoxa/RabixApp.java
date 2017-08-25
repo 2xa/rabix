eb76be81599035fe925dbd21b45910fe15336f63
AndiA92 <aczelandi@gmail.com>
2017-08-25 19:30:54 +0700

    Add swagger support

--- src/main/java/io/github/twoxa/RabixApp.java
+++ src/main/java/io/github/twoxa/RabixApp.java
@@ -2,9 +2,11 @@ package io.github.twoxa;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class RabixApp {
 
     public static void main(String[] args) throws Exception {
